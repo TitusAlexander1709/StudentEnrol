daa9007936136a4be12ebdcacdfd8953c56c4491
TitusAlexander1709 <greentag741@gmail.com>
2020-01-27 11:33:54 +0000

    First Committed version of Base MVC

---
+++ enrolSystem/grails-app/controllers/enrolsystem/CourseController.groovy
@@ -0,0 +1,7 @@
+package enrolsystem
+
+class CourseController {
+
+
+    def scaffold = Course 
+}
---
+++ enrolSystem/grails-app/domain/enrolsystem/Course.groovy
@@ -0,0 +1,29 @@
+package enrolsystem
+
+class Course {
+	String department;
+	String courseTitle;
+	String courseLeader;
+	String courseCode;
+	Date startDate;
+	Date endDate;
+	String description;
+	int numberOfStudents;
+	double tuitionFees;
+	String studyMode;
+
+
+    static constraints = {
+	courseTitle nullable: false, blank: false
+	department nullable: false, blank: false
+	courseLeader nullable: false, blank: false
+	courseCode nullable: false, blank: false
+	numberOfStudents nullable: false, blank: false, min: 20, max: 60
+	startDate nullable: false, blank: false
+	endDate nullable: false, blank: false
+	studyMode nullable: false, blank: false, minSize: 20
+	description nullable: false, blank: false, maxSize: 5000, widget:'textarea'
+	tuitionFees nullable: false, blank: false, scale: 2
+
+    }
+}
