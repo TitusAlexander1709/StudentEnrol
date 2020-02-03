efcd558bece50223ad7a64f59ec4df0a588c3d63
TitusAlexander1709 <greentag741@gmail.com>
2020-02-03 10:43:51 +0000

    Student class uploadad

---
+++ enrolSystem/grails-app/controllers/enrolsystem/ControllerStudentController.groovy
@@ -0,0 +1,6 @@
+package enrolsystem
+
+class ControllerStudentController {
+
+    def scaffold= Student
+}
---
+++ enrolSystem/src/test/groovy/enrolsystem/StudentControllerSpec.groovy
@@ -0,0 +1,18 @@
+package enrolsystem
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class StudentControllerSpec extends Specification implements DomainUnitTest<StudentController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
