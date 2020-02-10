9f1c2e155a32178de3207a574f59ba5e8450ee9d
TitusAlexander1709 <greentag741@gmail.com>
2020-02-10 10:55:53 +0000

    workingCourse class

--- enrolSystem/grails-app/domain/enrolsystem/Course.groovy
+++ enrolSystem/grails-app/domain/enrolsystem/Course.groovy
@@ -21,7 +21,7 @@ class Course {
 	numberOfStudents nullable: false, blank: false, min: 20, max: 60
 	startDate nullable: false, blank: false
 	endDate nullable: false, blank: false
-	studyMode nullable: false, blank: false, minSize: 20
+	studyMode nullable: false, blank: false, minSize: 7
 	description nullable: false, blank: false, maxSize: 5000, widget:'textarea'
 	tuitionFees nullable: false, blank: false, scale: 2
 
--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -3,6 +3,47 @@ package enrolsystem
 class BootStrap {
 
     def init = { servletContext ->
+	def newCourse=new Course(
+	department: 'Computing',
+	courseCode: 'CS123',
+	courseTitle: 'BSc Hon Computing',
+	courseLeader: 'Dr Michelle Murphy',
+	startDate:new Date ('09/09/2019'),
+	endDate:new Date ('07/07/2023'),
+	numberOfStudents: '55',
+	studyMode: 'Fulltime',
+	tuitionFees: '9000.60',
+	description: '"Placeholder text content."'
+	).save()
+
+	def compSci=new Course(
+	department: 'Computing',
+	courseCode: 'CS456',
+	courseTitle: 'BSc Hon Computer Science',
+	courseLeader: 'Dr Science',
+	startDate:new Date ('09/09/2019'),
+	endDate:new Date ('07/07/2023'),
+	numberOfStudents: '47',
+	studyMode: 'Fulltime',
+	tuitionFees: '9000.60',
+	description: '"Placeholder text content."'
+	).save()
+
+	def compNet=new Course(
+	department: 'Computing',
+	courseCode: 'CS987',
+	courseTitle: 'BSc Hon Networking',
+	courseLeader: 'Dr Networkman',
+	startDate:new Date ('09/09/2019'),
+	endDate:new Date ('07/07/2023'),
+	numberOfStudents: '48',
+	studyMode: 'Fulltime',
+	tuitionFees: '9000.60',
+	description: '"Placeholder text content."'
+	).save()
+
+
+
     }
     def destroy = {
     }
